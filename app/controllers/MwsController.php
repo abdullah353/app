260933c7486c61172bae54307a25cee73c9f9b0c
Abdullah <mabdullah353@gmail.com>
2014-08-14 17:45:43 +0500

    Boxc With Orders page from MWS

--- app/controllers/MwsController.php
+++ app/controllers/MwsController.php
@@ -12,50 +12,54 @@ class MwsController extends \BaseController {
 		$resp = null;
 		$orders = null;
 		$orderlist = array();
+		$orderitems = array();
+		$products = array();
 		$token = null;
 		$status = Input::get('status');
 		if($origDate = Input::get('from')){
 			$newDate = gmdate("Y-m-d\TH:i:s\Z", strtotime($origDate));
 
-			$url   = "https://mws.amazonservices.com/Orders/2013-09-01";
-			$url  .= "?Action=ListOrders";
-			$url  .= "&MarketplaceId.Id.1=ATVPDKIKX0DER";
-			$url  .= "&SellerId=A013695218D3GFTNCCXMX";
-			$url  .= "&SignatureVersion=2";
-			$url  .= "&SignatureMethod=HmacSHA256";
-			$url  .= "&CreatedAfter=".$newDate;
+			$url	 = "https://mws.amazonservices.com/Orders/2013-09-01";
+			$url	.= "?Action=ListOrders";
+			$url	.= "&MarketplaceId.Id.1=ATVPDKIKX0DER";
+			$url	.= "&SellerId=A013695218D3GFTNCCXMX";
+			$url	.= "&SignatureVersion=2";
+			$url	.= "&SignatureMethod=HmacSHA256";
+			$url	.= "&CreatedAfter=".$newDate;
+			
+
 			switch ($status) {
 				case 'PendingAvailability':
-					$url  .= "&OrderStatus.Status.1=PendingAvailability";
+					$url	.= "&OrderStatus.Status.1=PendingAvailability";
 					break;
 				case 'Pending':
-					$url  .= "&OrderStatus.Status.1=Pending";
+					$url	.= "&OrderStatus.Status.1=Pending";
 					break;
 				case 'Unshipped':
-					$url  .= "&OrderStatus.Status.1=Unshipped";
-					$url  .= "&OrderStatus.Status.2=PartiallyShipped";
+					$url	.= "&OrderStatus.Status.1=Unshipped";
+					$url	.= "&OrderStatus.Status.2=PartiallyShipped";
 					break;
 				case 'PartiallyShipped':
-					$url  .= "&OrderStatus.Status.1=Unshipped";
-					$url  .= "&OrderStatus.Status.2=PartiallyShipped";
+					$url	.= "&OrderStatus.Status.1=Unshipped";
+					$url	.= "&OrderStatus.Status.2=PartiallyShipped";
 					break;
 				case 'Shipped':
-					$url  .= "&OrderStatus.Status.1=Shipped";
+					$url	.= "&OrderStatus.Status.1=Shipped";
 					break;
 				case 'InvoiceUnconfirmed':
-					$url  .= "&OrderStatus.Status.1=InvoiceUnconfirmed";
+					$url	.= "&OrderStatus.Status.1=InvoiceUnconfirmed";
 					break;
 				case 'Canceled':
-					$url  .= "&OrderStatus.Status.1=Canceled";
+					$url	.= "&OrderStatus.Status.1=Canceled";
 					break;
 				case 'Unfulfillable':
-					$url  .= "&OrderStatus.Status.1=Unfulfillable";
+					$url	.= "&OrderStatus.Status.1=Unfulfillable";
 					break;
 			}
 
 			$sercretk = "XcWYM+HpwE2yrbo7SL4MQ1ONYtNF0kwOdbPjyeB9";
 			$accessid = "AKIAJUYJSUM5MS6FNFYA";
-			echo $this->getRequest($sercretk,$url,$accessid);
+			// echo $this->getRequest($sercretk,$url,$accessid);
 			$resp = $this->MakeRequest($this->getRequest($sercretk,$url,$accessid));
 			//print_r($this->MakeRequest($this->getRequest($sercretk,$url,$accessid)));
 			//return View::make('mws.index');
@@ -67,62 +71,139 @@ class MwsController extends \BaseController {
 				$orders = (isset($orders['Order'] ))? $orders['Order']:null;
 				if(gettype($orders) == "object"){array_push($orderlist, $orders);}else{$orderlist = $orders;}
 			}
-			
+
+
+			//**
+			$orderitems = array();
+			$allasin = array();
+			$allasin = array();
+			$asinlist = array();
+			foreach (array_chunk($orderlist, 10) as $orderlistchunk) {
+				foreach ($orderlistchunk as $order) {
+					$url	= "https://mws.amazonservices.com/Orders/2013-09-01";
+					$url .= "?Action=ListOrderItems";
+					$url .= "&SellerId=A013695218D3GFTNCCXMX";
+					$url .= "&AmazonOrderId=".$order->AmazonOrderId;
+					$url .= "&SignatureVersion=2";
+					$url .= "&SignatureMethod=HmacSHA256";
+					// echo $this->getRequest($sercretk,$url,$accessid);
+					$xmlresp = $this->MakeRequest($this->getRequest($sercretk,$url,$accessid));
+					$resp = json_decode(json_encode($xmlresp), true);
+					$orderitems["".$order->AmazonOrderId] = $resp["ListOrderItemsResult"];
+
+					// Getting Original Orders
+					foreach( $resp["ListOrderItemsResult"]["OrderItems"] as $orderitem ){
+						array_push($asinlist, $orderitem["ASIN"] );
+					}
+
+					// echo implode("&", $asinlist);
+					// echo '<pre>';
+					// print_r( $resp->ListOrderItemsResult );
+					// $order->orderitems = array();
+					// $json = json_encode($resp->ListOrderItemsResult );
+					// echo '</pre>';
+					// array_push($order->orderitems, $resp->ListOrderItemsResult);//$order->orderitems = json_decode($json, true);
+					// break;
+				}
+				// echo "Next Ten";
+			}//'ASINList.ASIN.'.++$loopcounter.'='.
+			$uniqueasins = array_unique( $asinlist );
+			foreach (array_chunk($uniqueasins, 10) as $asins) {
+				$url	= "https://mws.amazonservices.com/Products/2011-10-01";
+				$url .= "?Action=GetMatchingProduct";
+				$url .= "&SellerId=A013695218D3GFTNCCXMX";
+				$url .= "&MarketplaceId=ATVPDKIKX0DER";
+				$url .= "&SignatureVersion=2";
+				$url .= "&SignatureMethod=HmacSHA256";
+				for ($i=0; $i < count($asins); $i++) { 
+					$url .= '&ASINList.ASIN.'.( $i+1 ).'='.$asins[$i];
+				}
+				// echo '<pre>';
+				$xmlresp = $this->MakeRequest( $this->getRequest($sercretk,$url,$accessid, "2011-10-01" ) );
+				foreach ($xmlresp as $matcheditem) {
+					// print_r($matcheditem->attributes());
+					$currentasin = "";
+					foreach ($matcheditem->attributes() as $key => $value) {
+						if($key == "ASIN"){
+							$currentasin = $value;
+						}
+					}
+					// echo $currentasin."<br>";
+					if( isset( $matcheditem->Product ) ){
+						$products["".$currentasin] = array( "image"	=> preg_replace("/._(.*)?_./", ".", $matcheditem->Product->AttributeSets->children('ns2', true)->ItemAttributes->SmallImage->URL)
+											,"height" => (string) $matcheditem->Product->AttributeSets->children('ns2', true)->ItemAttributes->PackageDimensions->Height
+											,"width"	=> (string) $matcheditem->Product->AttributeSets->children('ns2', true)->ItemAttributes->PackageDimensions->Width
+											,"length" => (string) $matcheditem->Product->AttributeSets->children('ns2', true)->ItemAttributes->PackageDimensions->Length
+											,"weight" => (string) $matcheditem->Product->AttributeSets->children('ns2', true)->ItemAttributes->PackageDimensions->Weight);
+					}
+				}
+				// echo '</pre>';
+			}
+			// $url .= "&".implode("&", $asinlist);
+			// echo $url;
+
+				// print_r( $orderitems );
+			// print_r($orderlist);
 		}
 		//print_r($orderlist);
-		return View::make('mws.index')->with('orders',$orderlist)->with('token',$token)->withInput(Input::all());
+		return View::make('mws.index')
+			->with('orders',$orderlist)
+			->with('token',$token)
+			->with('orderitems',$orderitems)
+			->with('products',$products)
+			->withInput(Input::all());
 	}
 
 	/**
-	  * This function will take an existing Amazon request and change it so that it will be usable 
-	  * with the new authentication.
-	  *
-	  * @param string $secret_key - your Amazon AWS secret key
-	  * @param string $request - your existing request URI
-	  * @param string $access_key - your Amazon AWS access key
-	  * @param string $version - (optional) the version of the service you are using
-	  */
+		* This function will take an existing Amazon request and change it so that it will be usable 
+		* with the new authentication.
+		*
+		* @param string $secret_key - your Amazon AWS secret key
+		* @param string $request - your existing request URI
+		* @param string $access_key - your Amazon AWS access key
+		* @param string $version - (optional) the version of the service you are using
+		*/
 	protected function getRequest($secret_key, $request, $access_key = false, $version = '2013-09-01') {
-	    // Get a nice array of elements to work with
-	    $uri_elements = parse_url($request);
+			// Get a nice array of elements to work with
+			$uri_elements = parse_url($request);
 	 
-	    // Grab our request elements
-	    $request = $uri_elements['query'];
-	    $parameters = array();
-	    foreach (explode("&", $request) as $st) {
-	      $inter = explode("=", $st);
-	      $parameters[$inter[0]]= $inter[1];
-	    }
+			// Grab our request elements
+			$request = $uri_elements['query'];
+			$parameters = array();
+			foreach (explode("&", $request) as $st) {
+				$inter = explode("=", $st);
+				$parameters[$inter[0]]= $inter[1];
+			}
 
-	    // Add the new required paramters
-	    $parameters['Timestamp'] = gmdate("Y-m-d\TH:i:s\Z");
-	    $parameters['Version'] = $version;
-	    if (strlen($access_key) > 0) {
-	        $parameters['AWSAccessKeyId'] = $access_key;
-	    }   
+			// Add the new required paramters
+			$parameters['Timestamp'] = gmdate("Y-m-d\TH:i:s\Z");
+			$parameters['Version'] = $version;
+			if (strlen($access_key) > 0) {
+					$parameters['AWSAccessKeyId'] = $access_key;
+			}	 
 	 
-	    // The new authentication requirements need the keys to be sorted
-	    ksort($parameters);
-	    //print_r($parameters);
-	    // Create our new request
-	    foreach ($parameters as $parameter => $value) {
-	        // We need to be sure we properly encode the value of our parameter
-	        $parameter = str_replace("%7E", "~", rawurlencode($parameter));
-	        $value = str_replace("%7E", "~", rawurlencode($value));
-	        $request_array[] = $parameter . '=' . $value;
-	    }   
+			// The new authentication requirements need the keys to be sorted
+			ksort($parameters);
+			//print_r($parameters);
+			// Create our new request
+			foreach ($parameters as $parameter => $value) {
+					// We need to be sure we properly encode the value of our parameter
+					$parameter = str_replace("%7E", "~", rawurlencode($parameter));
+					$value = str_replace("%7E", "~", rawurlencode($value));
+					$request_array[] = $parameter . '=' . $value;
+			}	 
 	 
-	    // Put our & symbol at the beginning of each of our request variables and put it in a string
-	    $new_request = implode('&', $request_array);
+			// Put our & symbol at the beginning of each of our request variables and put it in a string
+			$new_request = implode('&', $request_array);
 	 
-	    // Create our signature string
-	    $signature_string = "GET\n{$uri_elements['host']}\n{$uri_elements['path']}\n{$new_request}";
+			// Create our signature string
+			$signature_string = "GET\n{$uri_elements['host']}\n{$uri_elements['path']}\n{$new_request}";
 	 
-	    // Create our signature using hash_hmac
-	    $signature = urlencode(base64_encode(hash_hmac('sha256', $signature_string, $secret_key, true)));
+			// Create our signature using hash_hmac
+			$signature = urlencode(base64_encode(hash_hmac('sha256', $signature_string, $secret_key, true)));
 	 
-	    // Return our new request
-	    return "https://{$uri_elements['host']}{$uri_elements['path']}?{$new_request}&Signature={$signature}";
+			// Return our new request
+			return "https://{$uri_elements['host']}{$uri_elements['path']}?{$new_request}&Signature={$signature}";
 	}
 
 	protected function MakeRequest( $url ){
@@ -135,15 +216,65 @@ class MwsController extends \BaseController {
 		// Interpret data as XML
 		$parsedXml = simplexml_load_string( $response );
 		return $parsedXml;
-  }
+	}
 
-  public function postExcel(){
-  	
-  	Excel::create('Filename', function($excel) {
+	public function postExcel(){
+		$maporder = Input::get('maporder');
+		$info = Excel::create( str_random(6), function($excel) {
 			$excel->sheet('Boxc', function($sheet) {
-	        $sheet->fromArray(Input::get('order'));
-	    });  		
-		})->export('csv');
-  }
+					$sheet->fromArray(Input::get('order'));
+			});			
+		})->store('xls', public_path().'/excel', true);
+		
+		$headers = array(
+			"Content-type"=>"text/html",
+			"Content-Disposition"=>"attachment;Filename=myfile.doc"
+		);
+		$content = '<html><head><meta charset="utf-8"></head><body>';
+		$content .= url('excel/'.$info['file'], $parameters = array(), $secure = null);
+		$content .= "<table><thead><tr>";
+		$content .= "<td>Orderid</td><td>image</td><td>Quantity</td><td>Name</td><td>PostalCode</td><td>State</td><td>City</td><td>Phone</td><td>Street1</td><td>Street2</td>";
+		$content .= "</tr></thead><tbody>";
+		foreach ($maporder as $order) {
+			$content .= '<tr>';
+			$content .= "<td>".$order["OrderID"]."</td>";
+			$content .= "<td><img src=".$order["image"]." style='height:75px;width:75px;'></td>";
+			$content .= "<td>".$order["Items"]."</td>";
+			$content .= "<td>".$order["Name"]."</td>";
+			$content .= "<td>".$order["PostalCode"];
+			$content .= "<td>".$order["State"]."</td>";
+			$content .= "<td>".$order["City"]."</td>";
+			$content .= "<td>".$order["Phone"]."</td>";
+			$content .= "<td>".$order["Street1"]."</td>";
+			$content .= "<td>".$order["Street2"]."</td>";
+	 		$content .= '</tr>';
+		}
+		$content .= '</tbody></table>';
+		$content .= '</body></html>';
+/*		echo '<pre>';
+		print_r(Input::all());
+		echo '</pre>';*/
+		return Response::make($content,200, $headers);
+	}
+
+	public function getWord(){
+	$headers = array(
+		"Content-type"=>"text/html",
+		"Content-Disposition"=>"attachment;Filename=myfile.doc"
+	);
+	$content = '<html>
+							<head>
+							<meta charset="utf-8">
+							</head>
+							<body>
+									<p>
+											My Content
+									</p>
+									<img src="http://ecx.images-amazon.com/images/I/41yVzXO4bRL.jpg">
+							</body>
+							</html>';
+	return Response::make($content,200, $headers);
+
+	}
 }
 
--- app/views/mws/index.blade.php
+++ app/views/mws/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="page-header">
-  <div class="icon">
-    <span class="ico-arrow-right"></span>
-  </div>
-  <h1>Orders/Transaction Page<small>Generate New orders</small></h1>
+	<div class="icon">
+		<span class="ico-arrow-right"></span>
+	</div>
+	<h1>Orders/Transaction Page<small>Generate New orders</small></h1>
 </div><!-- end .page-header -->
 
 {{ Form::open(array('action' => 'MwsController@getIndex', 'method' => 'get')) }}
@@ -18,7 +18,7 @@
 	<div class="row-form"><!-- new form row -->
 			<div class="span6"><!-- new form column -->
 				<span class="top">Fetch Orders From</span><!-- top text line -->
-				<input name="from" type="text" class="datepicker" value="{{ date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")-14, date("Y"))) }}" />   
+				<input name="from" type="text" class="datepicker" value="{{ date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")-14, date("Y"))) }}" />	 
 			</div>
 			<div class="span6"><!-- new form column -->
 				<span class="top">Other Details</span><!-- top text line --> 
@@ -55,57 +55,88 @@
 					<div class="span3">
 						<div class="block" id="sWidget_2" style="position: relative;">
 								<div class="head dblue">
-						    	<h6>MWS OrderId: {{ $el->AmazonOrderId }}</h6>
+									<h6>MWS OrderId: {{ $el->AmazonOrderId }}</h6>
 								</div>
 								@if(!empty($el->OrderTotal))
 									<div class="data dark">
 										AmountPaid: {{ $el->OrderTotal->Amount }}<br>
 										Shipped: {{ $el->NumberOfItemsShipped }} :: Unshipped: {{ $el->NumberOfItemsUnshipped }}<br>
+										@foreach( $orderitems["".$el->AmazonOrderId]["OrderItems"] as $orderitem )
+											<h6>{{ $orderitem["Title"] }}</h6>
+											<h6>Item Price {{ $orderitem["ItemPrice"]["Amount"] }}</h6>
+											<?php $currentasin = $orderitem["ASIN"]; ?>
+										@endforeach
+										<img src={{ $products["".$currentasin]["image"] }} >
 										<a href="#{{ $el->AmazonOrderId }}" role="button" class="btn" data-toggle="modal">OverView</a>
 									</div>
 								@endif
 						</div>
 					</div>
 					<div id="{{ $el->AmazonOrderId }}" class="modal hide fade" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h3 id="myModalLabel">{{ $el->AmazonOrderId }}</h3>
-            </div>
-            <div class="modal-body">
-                Shipped: {{ $el->NumberOfItemsShipped }} :: Unshipped: {{ $el->NumberOfItemsUnshipped }}<br>
-                @if( !empty($el->ShippingAddress) )
-	                Name: {{ $el->ShippingAddress->Name }}<br>
-	                CityName: {{ $el->ShippingAddress->City }}<br>
-	               	Country: {{ $el->ShippingAddress->Country }}<br>
-	               	CountryCode: {{ $el->ShippingAddress->CountryCode }}<br>
-	               	StateOrRegion: {{ $el->ShippingAddress->StateOrRegion }}<br>
-	               	Phone: {{ $el->ShippingAddress->Phone }}<br>
-	               	PostalCode: {{ $el->ShippingAddress->PostalCode }}<br>
-	               	AddressLine1: {{ $el->ShippingAddress->AddressLine1 }}<br>
-	               	AddressLine2: {{ $el->ShippingAddress->AddressLine2 }}<br>
-<input type="hidden" name="order[{{ $i }}][CompanyID]" value="">
-<input type="hidden" name="order[{{ $i }}][OrderID]" value="{{ $i }}">
-<input type="hidden" name="order[{{ $i }}][SKU]" value="">
-<input type="hidden" name="order[{{ $i }}][Service]" value="">
-<input type="hidden" name="order[{{ $i }}][Name]" value="{{ $el->ShippingAddress->Name }}">
-<input type="hidden" name="order[{{ $i }}][Phone]" value="{{ $el->ShippingAddress->Phone }}">
-<input type="hidden" name="order[{{ $i }}][Street1]" value="{{ $el->ShippingAddress->AddressLine1 }}">
-<input type="hidden" name="order[{{ $i }}][Street2]" value="{{ $el->ShippingAddress->AddressLine2 }}">
-<input type="hidden" name="order[{{ $i }}][City]" value="{{ $el->ShippingAddress->City }}">
-<input type="hidden" name="order[{{ $i }}][State]" value="{{ $el->ShippingAddress->StateOrRegion }}">
-<input type="hidden" name="order[{{ $i }}][PostalCode]" value="{{ $el->ShippingAddress->PostalCode }}">
-<input type="hidden" name="order[{{ $i }}][Contents]" value="">
-<input type="hidden" name="order[{{ $i }}][Items]" value="{{ $el->NumberOfItemsUnshipped }}">
-<input type="hidden" name="order[{{ $i }}][Value]" value="">
-<input type="hidden" name="order[{{ $i }}][SignatureConfirmation]" value="">
-<input type="hidden" name="order[{{ $i }}][Units]" value="">
-<input type="hidden" name="order[{{ $i }}][Weight]" value="">
-<input type="hidden" name="order[{{ $i }}][Height]" value="">
-<input type="hidden" name="order[{{ $i }}][Width]" value="">
-<input type="hidden" name="order[{{ $i }}][Depth]" value="">
-               	@endif
-            </div>
-        </div>
+						<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								<h3 id="myModalLabel">{{ $el->AmazonOrderId }}</h3>
+						</div>
+						<div class="modal-body">
+								Shipped: {{ $el->NumberOfItemsShipped }} :: Unshipped: {{ $el->NumberOfItemsUnshipped }}<br>
+								@if( !empty($el->ShippingAddress) )
+									Name: {{ $el->ShippingAddress->Name }}<br>
+									CityName: {{ $el->ShippingAddress->City }}<br>
+								 	Country: {{ $el->ShippingAddress->Country }}<br>
+								 	CountryCode: {{ $el->ShippingAddress->CountryCode }}<br>
+								 	StateOrRegion: {{ $el->ShippingAddress->StateOrRegion }}<br>
+								 	Phone: {{ $el->ShippingAddress->Phone }}<br>
+								 	PostalCode: {{ $el->ShippingAddress->PostalCode }}<br>
+								 	AddressLine1: {{ $el->ShippingAddress->AddressLine1 }}<br>
+								 	AddressLine2: {{ $el->ShippingAddress->AddressLine2 }}<br>
+								 	<hr>
+								 	<pre>
+								 		{{ print_r( $el ) }}
+								 	</pre><hr>
+								 	<pre>
+								 		{{ print_r( $orderitems["".$el->AmazonOrderId] ) }}
+								 	</pre><hr>
+								 	<pre>
+								 		{{ print_r( $products["".$currentasin] ) }}
+								 	</pre>
+								 	<?php $randOrder = str_random(6); ?>
+<input type="hidden" name="order[{{ $randOrder }}][CompanyID]" value="838">
+<input type="hidden" name="order[{{ $randOrder }}][OrderID]" value="{{ $randOrder }}">
+<input type="hidden" name="order[{{ $randOrder }}][SKU]" value="">
+<input type="hidden" name="order[{{ $randOrder }}][Service]" value="">
+<input type="hidden" name="order[{{ $randOrder }}][Name]" value="{{ $el->ShippingAddress->Name }}">
+<input type="hidden" name="order[{{ $randOrder }}][Phone]" value="{{ $el->ShippingAddress->Phone }}">
+<input type="hidden" name="order[{{ $randOrder }}][Street1]" value="{{ $el->ShippingAddress->AddressLine1 }}">
+<input type="hidden" name="order[{{ $randOrder }}][Street2]" value="{{ $el->ShippingAddress->AddressLine2 }}">
+<input type="hidden" name="order[{{ $randOrder }}][City]" value="{{ $el->ShippingAddress->City }}">
+<input type="hidden" name="order[{{ $randOrder }}][State]" value="{{ $el->ShippingAddress->StateOrRegion }}">
+<input type="hidden" name="order[{{ $randOrder }}][PostalCode]" value="{{ $el->ShippingAddress->PostalCode }}">
+<input type="hidden" name="order[{{ $randOrder }}][Contents]" value="">
+<input type="hidden" name="order[{{ $randOrder }}][Items]" value="{{ $el->NumberOfItemsUnshipped }}">
+<input type="hidden" name="order[{{ $randOrder }}][Value]" value="{{ $orderitem['ItemPrice']['Amount'] }}">
+<input type="hidden" name="order[{{ $randOrder }}][SignatureConfirmation]" value="0">
+<input type="hidden" name="order[{{ $randOrder }}][Units]" value="Metric">
+<input type="hidden" name="order[{{ $randOrder }}][Weight]" value="{{ $products[''.$currentasin]['weight'] }}">
+<input type="hidden" name="order[{{ $randOrder }}][Height]" value="{{ $products[''.$currentasin]['height'] }}">
+<input type="hidden" name="order[{{ $randOrder }}][Width]" value="{{ $products[''.$currentasin]['width'] }}">
+<input type="hidden" name="order[{{ $randOrder }}][Depth]" value="1">
+
+
+<input type="hidden" name="maporder[{{ $randOrder }}][OrderID]" value="{{ $randOrder }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][image]" value="{{ $products["".$currentasin]['image'] }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][Items]" value="{{ $el->NumberOfItemsUnshipped }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][PostalCode]" value="{{ $el->ShippingAddress->PostalCode }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][State]" value="{{ $el->ShippingAddress->StateOrRegion }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][City]" value="{{ $el->ShippingAddress->City }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][Street2]" value="{{ $el->ShippingAddress->AddressLine2 }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][Street1]" value="{{ $el->ShippingAddress->AddressLine1 }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][Phone]" value="{{ $el->ShippingAddress->Phone }}">
+<input type="hidden" name="maporder[{{ $randOrder }}][Name]" value="{{ $el->ShippingAddress->Name }}">
+
+
+							 	@endif
+						</div>
+				</div>
 				@endforeach
 			</div>
 		@endforeach
